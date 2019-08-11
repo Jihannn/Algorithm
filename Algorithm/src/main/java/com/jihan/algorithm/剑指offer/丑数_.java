616fad6fb9b35b41e5646346c6f51b0052974cdc
gjihan <gjihan97@gmail.com>
2019-08-11 20:17:32 +0800

    copy

--- Algorithm/src/main/java/com/jihan/algorithm/剑指offer/丑数_.java
+++ Algorithm/src/main/java/com/jihan/algorithm/剑指offer/丑数_.java
@@ -3,12 +3,12 @@ package com.jihan.algorithm.剑指offer;
 /**
  * @author Jihan
  * @date 2019/8/10
- * <p>
+ *
  * 把只包含质因子2、3和5的数称作丑数（Ugly Number）。
  * 例如6、8都是丑数，但14不是，因为它包含质因子7。
  * 习惯上我们把1当做是第一个丑数。求按从小到大的顺序的第N个丑数。
  */
-public class _丑数 {
+public class 丑数_ {
 
     final int[] fix = {2, 3, 5};
 
