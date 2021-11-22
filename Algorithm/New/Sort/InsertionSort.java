0d4e141481b2b46c94146e09a6d419bb0b5fba00
Jihan <gjihan97@gmail.com>
2021-11-22 23:15:31 +0800

    update:InsetionSort

--- Algorithm/New/Sort/InsertionSort.java
+++ Algorithm/New/Sort/InsertionSort.java
@@ -1,37 +1,97 @@
-public class InsertionSort{
-    
-    public static void insertionSort(int[] list){
-        if(list == null || list.length < 2){
+public class InsertionSort {
+
+    // 直接插入排序
+    public static void insertionSort(int[] array) {
+        if (array == null || array.length < 2) {
             return;
         }
-        for (int i = 1; i < list.length; i++) {
-            for (int j = i;j > 0 && list[j] < list[j-1]; j--) {
-                swap(list,j,j-1);
+        for (int i = 1; i < array.length; i++) {
+            for (int j = i; j > 0 && array[j] < array[j - 1]; j--) {
+                swap(array, j, j - 1);
             }
         }
     }
 
-    public static void swap(int list[],int i,int j){
-        int temp = list[i];
-        list[i] = list[j];
-        list[j] = temp;
+    // 哨兵插入排序
+    public static void sentryInsertionSort(int[] array) {
+        if (array == null || array.length < 2) {
+            return;
+        }
+        // 第一个位置留给哨兵
+        int[] newArray = new int[array.length + 1];
+        System.arraycopy(array, 0, newArray, 1, array.length);
+        for (int i = 2; i < newArray.length; i++) {
+            // 如果前面已经有序则不遍历
+            if (newArray[i] < newArray[i - 1]) {
+                // 当前位置赋值给哨兵
+                newArray[0] = newArray[i];
+                int j = i - 1;
+                // 相比直接插入少一次比较
+                for (; newArray[j] > newArray[0]; j--) {
+                    swap(newArray, j, j + 1);
+                }
+                newArray[j + 1] = newArray[0];
+            }
+        }
+        // 赋值回原数组
+        System.arraycopy(newArray, 1, array, 0, array.length);
     }
-    
-    public static void printList(int list[]){
-        for (int i = 0; i < list.length; i++) {
-            if(i == 0){
-                System.out.print("["); 
-            }else if(i == list.length-1){
-                System.out.print(list[i]+"]");
+
+    // 折半插入排序
+    public static void halfInsertionSort(int[] array) {
+        if (array == null || array.length < 2) {
+            return;
+        }
+        int[] newArray = new int[array.length + 1];
+        System.arraycopy(array, 0, newArray, 1, array.length);
+        for (int i = 2; i < newArray.length; i++) {
+            int low = 1;
+            int high = i - 1;
+            newArray[0] = newArray[i];
+            while (low <= high) {
+                int mid = (low + high) / 2;
+                if (newArray[0] < newArray[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            }
+            // 最后插入位置为high + 1
+            for (int j = i - 1; j >= high + 1; j--) {
+                newArray[j + 1] = newArray[j];
+            }
+            newArray[high + 1] = newArray[0];
+        }
+        System.arraycopy(newArray, 1, array, 0, array.length);
+    }
+
+    public static void swap(int array[], int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    public static void printArray(int array[]) {
+        for (int i = 0; i < array.length; i++) {
+            if (i == 0) {
+                System.out.print("[");
+            } else if (i == array.length - 1) {
+                System.out.print(array[i] + "]");
                 break;
             }
-            System.out.print(list[i]+",");
+            System.out.print(array[i] + ",");
         }
     }
 
     public static void main(String[] args) {
-        int list[] = {5,4,3,2,1};
-        insertionSort(list);
-        printList(list);
+        int[] array1 = { 0, 6, 1, 3, 8, 4, 0, 3 };
+        insertionSort(array1);
+        printArray(array1);
+        int[] array2 = { 0, 6, 1, 3, 8, 4, 0, 3 };
+        InsertionSort.sentryInsertionSort(array2);
+        printArray(array2);
+        int[] array3 = { 0, 6, 1, 3, 8, 4, 0, 3 };
+        InsertionSort.halfInsertionSort(array3);
+        printArray(array2);
     }
 }
