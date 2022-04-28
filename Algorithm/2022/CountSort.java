3e7aa5c3140fad316b9e9f7984c2be05fbe7b88f
Jihan <gjihan97@gmail.com>
2022-04-28 14:27:49 +0800

    计数排序,基数排序,前缀树

---
+++ Algorithm/2022/CountSort.java
@@ -0,0 +1,103 @@
+import java.util.Arrays;
+
+/*
+ * @Author: Jihan
+ * @Date: 2022-04-28 11:26:24
+ * @Description: 计数排序
+ */
+public class CountSort {
+    public static void countSort(int[] arr){
+        if(arr == null || arr.length < 2){
+            return;
+        }
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            max = Math.max(max, arr[i]);
+        }
+        int[] bucket = new int[max + 1];
+        for (int i = 0; i < arr.length; i++) {
+            bucket[arr[i]] += 1;
+        }
+        int index = 0;
+        for (int i = 0; i < bucket.length; i++) {
+            while(bucket[i]-- > 0){
+                arr[index++] = i;
+            }
+        }
+    }
+    
+    // for test
+	public static void comparator(int[] arr) {
+		Arrays.sort(arr);
+	}
+
+	// for test
+	public static int[] generateRandomArray(int maxSize, int maxValue) {
+		int[] arr = new int[(int) ((maxSize + 1) * Math.random())];
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = (int) ((maxValue + 1) * Math.random());
+		}
+		return arr;
+	}
+
+	// for test
+	public static int[] copyArray(int[] arr) {
+		if (arr == null) {
+			return null;
+		}
+		int[] res = new int[arr.length];
+		for (int i = 0; i < arr.length; i++) {
+			res[i] = arr[i];
+		}
+		return res;
+	}
+
+	// for test
+	public static boolean isEqual(int[] arr1, int[] arr2) {
+		if ((arr1 == null && arr2 != null) || (arr1 != null && arr2 == null)) {
+			return false;
+		}
+		if (arr1 == null && arr2 == null) {
+			return true;
+		}
+		if (arr1.length != arr2.length) {
+			return false;
+		}
+		for (int i = 0; i < arr1.length; i++) {
+			if (arr1[i] != arr2[i]) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	// for test
+	public static void printArray(int[] arr) {
+		if (arr == null) {
+			return;
+		}
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+		System.out.println();
+	}
+
+	// for test
+	public static void main(String[] args) {
+		int testTime = 500000;
+		int maxSize = 100;
+		int maxValue = 150;
+		for (int i = 0; i < testTime; i++) {
+			int[] arr1 = generateRandomArray(maxSize, maxValue);
+			int[] arr2 = copyArray(arr1);
+			countSort(arr1);
+			comparator(arr2);
+			if (!isEqual(arr1, arr2)) {
+				printArray(arr1);
+				printArray(arr2);
+                System.out.println("error");
+				break;
+			}
+		}
+	}
+}
---
+++ Algorithm/2022/RadixSort.java
@@ -0,0 +1,70 @@
+/*
+ * @Author: Jihan
+ * @Date: 2022-04-28 11:45:45
+ * @Description: 基数排序
+ */
+public class RadixSort {
+    public static void radixSort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        radixSort(arr, 0, arr.length - 1, getMaxDigit(arr,0,arr.length-1));
+    }
+
+    public static void radixSort(int[] arr, int L, int R, int digit) {
+        for (int i = 1; i <= digit; i++) {
+            int[] count = new int[10];
+            int[] help = new int[R - L + 1];
+            // 所有数当前digit出现的次数
+            for (int j = L; j <= R; j++) {
+                count[getDigit(arr[j], i)]++;
+            }
+            // 前缀累加和，得到每个数位置的范围
+            for (int j = 1; j < count.length; j++) {
+                count[j] += count[j - 1];
+            }
+            // 复制到辅助数组指定位置
+            for (int j = R; j >= L; j--) {
+                help[--count[getDigit(arr[j], i)]] = arr[j];
+            }
+            // 按当前digit排好序，复制回原数组
+            for (int j = 0; j < help.length; j++) {
+                arr[j + L] = help[j];
+            }
+        }
+    }
+
+    public static int getMaxDigit(int[] arr,int L,int R) {
+        int maxDigit = 0;
+        int maxNum = Integer.MIN_VALUE;
+        for (int i = L; i <= R; i++) {
+            maxNum = Math.max(maxNum, arr[i]);
+        }
+        while (maxNum != 0) {
+            maxDigit++;
+            maxNum /= 10;
+        }
+        return maxDigit;
+    }
+
+    public static int getDigit(int num, int digit) {
+        return ((num / (int) Math.pow(10, digit - 1)) % 10);
+    }
+
+    public static void printArr(int[] arr){
+        System.out.print("[");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+            if(i != arr.length - 1){
+                System.out.print(",");
+            }
+        }
+        System.out.println("]");
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {73,2,40,638,9,123,56,79534,62,0,464};
+        radixSort(arr);
+        printArr(arr);
+    }
+}
